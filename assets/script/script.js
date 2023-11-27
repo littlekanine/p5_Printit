cfdc48a1fca1df94954966ff4db2d073b797c984
littlekanine <elia.kopff3891@gmail.com>
2023-11-27 17:44:13 +0100

    wip_3

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+let banner = document.getElementById("banner")
+console.log(banner)
+
+let arrow_left = document.querySelector(".arrow_left")
+console.log(arrow_left)
+
+let arrow_right = document.querySelector(".arrow_right")
+console.log(arrow_right)
